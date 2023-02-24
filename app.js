d08ea097b739bfe36eb60f95031a9bfd4998c211
Oluwaseun <oluwaseunadesina8@gmail.com>
2023-02-24 04:28:32 +0100

    update package json

---
+++ app.js
@@ -0,0 +1,41 @@
+const  express =  require ('express');
+const dotenv = require('dotenv')
+const  app =  express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
+
+app.get('/', (req, res) =>{
+    res.send('Hello World');
+}) 
+
+app.get('/api/courses', (req, res) =>{
+    res.send(courses);
+})
+ 
+app.get('/api/courses/:id', (req, res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found');
+    res.send(course);
+})
+
+app.post('/api/courses', (req, res) =>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+}) ;
